06d37f8af9965f2fe7458f7cc3174a81d45a05c0
Max P <maxmay@iastate.edu>
2024-01-28 23:34:51 -0600

    print map

--- Assignment1/Assignmet1.01.c
+++ Assignment1/Assignmet1.01.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 
+#define HEIGHT 21
+#define WIDTH 80
 
-
+void printMap(char map[HEIGHT][HEIGHT]){
+    int i;
+    int j;
+    for (i=0; i < HEIGHT; i++){
+        for(j=0; j < WIDTH; j++){
+            printf("%c", map[i][j]);
+        }
+        printf("\n");
+    }
+}
 
 
 
 int main(int argc, char *argv[]){
-    
+    char map[HEIGHT][WIDTH];
+
+    printMap(map);
+    return 0;
 }
