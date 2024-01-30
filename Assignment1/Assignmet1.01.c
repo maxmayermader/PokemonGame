d99330c0b7c9f21bc7d13d879543933205df72aa
Max P <maxmay@iastate.edu>
2024-01-29 23:57:37 -0600

    added random seeds

--- Assignment1/Assignmet1.01.c
+++ Assignment1/Assignmet1.01.c
@@ -4,6 +4,17 @@
 #define HEIGHT 21
 #define WIDTH 80
 
+#define BOULDER '%'
+#define TREE '^'
+#define ROAD '#'
+#define MART 'M'
+#define CENTER 'C'
+#define GRASS ':'
+#define CLEARING '.'
+#define WATER '~'
+//#define PLAYER '@'
+
+
 
 /////Que Implemntation////
 int queue[MAX_SIZE];  
@@ -32,12 +43,40 @@ int dequeue() {
     return element;  
 }  
 
-void createMap(map){
+
+
+
+
+//TODO 7 biomes
+//2 tall grass
+//1 water region
+//2 clearings
+//1 forrest
+//1 mountin
+void createMap(char *map[HEIGHT][HEIGHT]){
+    //assign biomes
+    int grass1[2][1] = {rand() % (WIDTH+1), rand() % (HEIGHT+1)};
+    int grass2[2][1] = {rand() % (WIDTH+1), rand() % (HEIGHT+1)};
+    int water[2][1] = {rand() % (WIDTH+1), rand() % (HEIGHT+1)};
+    int clearing1[2][1] = {rand() % (WIDTH+1), rand() % (HEIGHT+1)};
+    int clearing2[2][1] = {rand() % (WIDTH+1), rand() % (HEIGHT+1)};
+    int forrest[2][1] = {rand() % (WIDTH+1), rand() % (HEIGHT+1)};
+    int mountin[2][1] = {rand() % (WIDTH+1), rand() % (HEIGHT+1)};
+
+    /*
+    1. make map with que
+    2. make border
+    3. make roads
+    4. make buildings
+    
+    
+    */
+
 
 }
 
 //Prints map out to the terminal
-void printMap(char map[HEIGHT][HEIGHT]){
+void printMap(char *map[HEIGHT][HEIGHT]){
     int i;
     int j;
     for (i=0; i < HEIGHT; i++){
@@ -50,6 +89,9 @@ void printMap(char map[HEIGHT][HEIGHT]){
 
 int main(int argc, char *argv[]){
     char map[HEIGHT][WIDTH];
+
+    srand(time(NULL));
+
 
     createMap(map);
 
